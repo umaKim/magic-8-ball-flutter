c9bb999fe3b954e40af4ceae828f867dbb4f7321
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-12-29 10:40:17 +0900

    practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBall(),
       ),
     );
+
+class MagicBall extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text("Ask me something"),
+      ),
+      body: MagicBallStatefulWidget(),
+    );
+  }
+}
+
+class MagicBallStatefulWidget extends StatefulWidget {
+  @override
+  State<MagicBallStatefulWidget> createState() =>
+      _MagicBallStatefulWidgetState();
+}
+
+class _MagicBallStatefulWidgetState extends State<MagicBallStatefulWidget> {
+  int number = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {
+            setState(() {
+              number = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$number.png')
+        ),
+      ],
+    );
+  }
+}
